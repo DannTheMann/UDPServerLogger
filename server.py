88ee4ced8b8d7e6faa58e2692f0eea3747226cba
rob <em.em>
2019-06-26 17:17:24 +0100

    Init

---
+++ server.py
@@ -0,0 +1,41 @@
+import socket
+import datetime
+import os
+
+UDP_IP      = "0.0.0.0"
+UDP_PORT    = 3333
+FILE_PATH   = "logs/"
+
+def writeToFile(context):
+    print(context)
+    f = open(FILE_PATH + datetime.datetime.now().strftime("%Y-%m-%d") + ".log", "a")
+    f.write(context + "\n")
+    f.close()
+
+def udpServer():
+    print("Hosting UDP Server on port: {}".format(UDP_PORT))
+
+    if not os.path.exists(FILE_PATH):
+        print("File path '{}' did not exist, creating it.".format(FILE_PATH))
+        os.makedirs(FILE_PATH)   
+
+    sock = socket.socket(socket.AF_INET, # Internet
+                        socket.SOCK_DGRAM) # UDP
+    sock.bind((UDP_IP, UDP_PORT))
+
+    while True:
+        data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
+        msg = "{}: {}".format(addr[0], data)
+        writeToFile(msg)
+
+def __main__():
+    print("")
+    print("")
+    print(" ----------- ")
+    print("")
+    print("")
+    print("Starting application. Time: {}".format(datetime.datetime.now()))
+    udpServer()
+
+__main__()
+
